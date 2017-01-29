171e040e737db21c97fedf99292ff16e79537d7d
wrongpoison <mbavlsik@gmail.com>
2017-01-28 21:36:30 -0600

    fix bottle sizing, remove bottle cap

--- include/pills.js
+++ include/pills.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-	var bottle = $(".pill-bottle");
+	var bottle = $("#pillbottle");
+	var bottlecap = $(".cap");
 
 	TweenMax.to(bottle, 2, {rotation:"-140"});
+	TweenMax.to(bottlecap, 2, { y:"-500"});
 
 });
